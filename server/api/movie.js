c1ac33d1359eda2b575fda930d84d43e368228ed
Fahmi <itsfahmiefendy@gmail.com>
2024-01-29 11:29:03 +0700

    feat: add response message and status code

--- server/api/movie.js
+++ server/api/movie.js
@@ -6,7 +6,9 @@ const movieList = async (req, res) => {
   try {
     const response = await MovieHelper.getMovieList();
 
-    return res.send(response);
+    return res
+      .status(200)
+      .send({ message: "Successfully Get All Movie", data: response });
   } catch (err) {
     console.log(err.message, "<<< getMovieList Error");
   }
@@ -18,7 +20,10 @@ const movieListById = async (req, res) => {
   try {
     const response = await MovieHelper.getMovieListById(id);
 
-    return res.send(response);
+    return res.status(200).send({
+      message: `Successfully Get Movie with id of ${id}`,
+      data: response,
+    });
   } catch (err) {
     console.log(err.message, "<<< getMovieListById Error");
   }
@@ -28,7 +33,10 @@ const createMovie = async (req, res) => {
   try {
     const response = await MovieHelper.createMovie(req.body);
 
-    return res.send(response);
+    return res.status(201).send({
+      message: "Successfuly Created New Data!",
+      data: response,
+    });
   } catch (err) {
     console.log(err.message, "<<< createMovie Error");
   }
